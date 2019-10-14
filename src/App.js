809363c5d985af52459d148f8f10f2d877e58119
Nathaniel Mosco <natemosco@gmail.com>
2019-10-14 17:08:38 -0400

    initial changes to the Basic Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-
+import { Route } from "react-router-dom";
 const App = () => (
   <div>
-    <Navigation />
+    <Route exact path="/" component={Navigation} />
+    <Route path="/contact" component={Contact}></Route>
+    <Route path="/about" component={About}></Route>
   </div>
 );
 
